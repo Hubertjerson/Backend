b0cc2b0966d54de150f194702041db182953a2f2
hubertjerson <jerson.huaman.11@outlook.com>
2022-05-26 09:35:45 -0400

    Desafio02

--- Desafio 02/index.js
+++ Desafio 02/index.js
@@ -1,73 +1,24 @@
-    // LLAMAMOS AL ARCHIVO CONTARINER
+const Contenedor = require("./Contenedor");
 
-const Container = require('./container')
-const nuevoProducto = new Container('producto.txt')
+const contenedor = new Contenedor("productos.txt");
 
-    //MOSTRANDO MENU AL USUARIO DE OPCIONES
+const main = async () => {
+    //const id1 = await contenedor.save({
+    //    title: "Regla",
+    //    price: 75.66
+    //});
+    //console.log(id1);
 
-const mostrarMenu = () => {
-    setTimeout(() => {
-        console.log(`
-        Opciones:
-            1.save
-            2.getById
-            3.getAll
-            4.deleteById
-            5.deleteAll
-            6.exit
-`)
-    }, 1000)
-}
-mostrarMenu()
 
-function menu() {
-    let stdin = process.openStdin()
+    //const object2 = await contenedor.getById();
+    //console.log(object2);
 
-    stdin.addListener('data', function (d) {
-        let option = d.toString().trim()
+    //await contenedor.deleteById(2);
 
-        switch (option) {
-                // GUARDANDO Y MOSTRANDO ID GUARDADO DEL PRODUCTO
-            case '1':
-                const obj = {
-                    // PRODUCTOS AGREGANDO
-                    title: 'Productos Agregados',
-                    price: 34.5,
-                    thumbnail: 'Aqui Va una imagen del producto'
-                }
-                nuevoProducto.save(obj)
-                mostrarMenu()
-                break
-                //  MOSTRANDO ID GUARDADO DEL PRODUCTO
-            case '2':
-                const id = 1
-                console.log(nuevoProducto.getById(id))
-                mostrarMenu()
-                break
-                // MOSTRANDO UN ARRAY GUARDADO DEL PRODUCTO
-            case '3':
-                console.log(nuevoProducto.getAll())
-                mostrarMenu()
-                break
-                // ELIMINANDO ID ELEGIDO DEL PRODUCTO
-            case '4':
-                const id2 = 2
-                nuevoProducto.deleteById(id2)
-                mostrarMenu()
-                break
-                // ELIMINA TODO EL ARRAY PRODUCTO
-            case '5':
-                nuevoProducto.deleteAll()
-                mostrarMenu()
-                break
-                // SALIDA
-            case '6':
-                process.exit(0)
+    //const allCurrentObjects = await contenedor.getAll();
+    //console.log(allCurrentObjects);
 
-            default:
-                console.log('Error')
-                process.exit(0)
-        }
-    })
-}
-menu()
+    //await contenedor.deleteAll();
+};
+
+main();
