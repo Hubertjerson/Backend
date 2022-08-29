0d65b5f4162c0a24996146886a0ec5020a03f441
hubertjerson <jerson.huaman.11@outlook.com>
2022-08-28 22:55:35 -0400

    desafio16

---
+++ Desafio 16/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const minimist = require('minimist')
+
+const options = {
+    alias: {
+        "p": "PORT"
+    },
+    default: {
+        "PORT": 8080
+    }
+};
+
+const { PORT } = minimist(process.argv.slice(2), options);
+
+const productRouter = require('./routes/producto')
+const otherRouter = require('./routes/other')
+
+const log4js = require('./utils/logs');
+const loggerConsole = log4js.getLogger('default');
+const loggerArchiveWarn = log4js.getLogger('warnArchive');
+const loggerArchiveError = log4js.getLogger(`errorArchive`);
+
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/productos', productRouter);
+app.use('/test', otherRouter);
+
+app.get('/', (req, res) => {
+    res.send('DESAFIO 16 PROYECTO HEROKU FUNCIONANDO');
+});
+
+const server = app.listen(PORT, () => {
+    loggerConsole.info(`🚀 Server started at http://localhost:${PORT}`)
+    })
+
+server.on('error', (err) => loggerArchiveError.error(err));
