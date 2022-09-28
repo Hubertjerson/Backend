d482ad6e1cb742257485e6ce1185b3a019716bfb
hubertjerson <jerson.huaman.11@outlook.com>
2022-09-28 12:51:56 -0300

    Desafio 18

--- Desafio 18/src/test/api.test.js
+++ Desafio 18/src/test/api.test.js
@@ -3,47 +3,45 @@ const expect = require(`chai`).expect;
 
 //Test BDD:
 describe(`Test Api`, () => {
-    describe(`GET / `, () => {
-
-        it(`Debería retornar status 200`, async () => {
-            const response = await request.get(`/`);
-
-            expect(response.status).to.eql(200);
-        });
-
-        it(`Debería retornar un arreglo`, async () => {
-            const response = await request.get(`/`);
-
-            expect(typeof response._body).to.eql(`object`);
-        });
-    });
+    //describe(`GET / `, () => {
+//
+    //    it(`Debería retornar status 200`, async () => {
+    //        const response = await request.get(`/`);
+//
+    //        expect(response.status).to.eql(200);
+    //    });
+//
+    //    it(`Debería retornar un arreglo`, async () => {
+    //        const response = await request.get(`/`);
+//
+    //        expect(typeof response._body).to.eql(`object`);
+    //    });
+    //});
 
     describe(`POST /`, () => {
         it(`Debería agregar un producto`, async () => {
             const response = await request.post(`/`).send({
                 nombre: `producto desde supertest api`,
                 precio: 1234,
-                url: `URL desde supertest api`,
+                thumbnail: `URL desde supertest api`,
                 descripcion: `descripción desde supertest api`,
-                timestamp: new Date().toDateString(),
-                codigo: 33333,
-                stock: 3,
-                cantidad: 0
+                codigo: 123,
+                stock: 10,
             });
 
             expect(response.status).to.eql(201);
         });
     });
 
-    describe(`DELETE / `, () => {
-
-        it(`Debería retornar status 200`, async () => {
-            //Colocar id conocido de la DB
-            const response = await request.delete(`/630649420257d0ab34ff18c7`);
-
-            expect(response.status).to.eql(200);
-        });
-    });
+    //describe(`DELETE / `, () => {
+//
+    //    it(`Debería retornar status 200`, async () => {
+    //        //Colocar id conocido de la DB
+    //        const response = await request.delete(`/630649420257d0ab34ff18c7`);
+//
+    //        expect(response.status).to.eql(200);
+    //    });
+    //});
 
 
 });
